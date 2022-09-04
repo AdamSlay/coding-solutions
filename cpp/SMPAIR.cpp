60b3571ca0a5c118d4f54fd20009107045297a66
AdamSlay <adamslay11@gmail.com>
2022-09-04 02:35:28 -0500

    Code Chef

---
+++ cpp/SMPAIR.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+void solve()
+{
+    std::vector<int> a;
+    int n; std::cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int p;
+        std::cin >> p;
+        a.push_back(p);
+    }
+    sort(a.begin(), a.end());
+    std::cout << a[0] + a[1] << "\n";
+
+}
+int main(){
+    int t; std::cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
